07b99e00d5e030d3067f017f0db9d39ff2d11655
Michał Ordon <michal@tehidea.com>
2016-05-24 21:48:30 +0100

    Add router, and clean up

--- app/index.js
+++ app/index.js
@@ -1,70 +1,7 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-
-var USER_DATA = {
-  name: 'Michał Ordon',
-  username: 'designorant',
-  image: 'https://avatars2.githubusercontent.com/u/87477'
-}
-
-var ProfilePic = React.createClass({
-  render: function() {
-    return <img src={this.props.imageUrl} style={{height: 100, width: 100}} />
-  }
-})
-
-var Link = React.createClass({
-
-  changeURL: function() {
-    window.location.replace(this.props.href)
-  },
-
-  render: function() {
-    return (
-      <span style={{color: 'blue', cursor: 'pointer'}} onClick={this.changeURL}>
-        {this.props.children}
-      </span>
-    );
-  }
-
-});
-
-var ProfileLink = React.createClass({
-  render: function() {
-    return (
-      <div>
-        <Link href={'https://github.com/' + this.props.username}>
-          {this.props.username}
-        </Link>
-      </div>
-    )
-  }
-})
-
-var ProfileName = React.createClass({
-  render: function() {
-    return (
-      <div>
-        {this.props.name}
-      </div>
-    )
-  }
-})
-
-var Avatar = React.createClass({
-
-  render: function() {
-    return (
-      <div>
-        <ProfilePic imageUrl={this.props.user.image} />
-        <ProfileName name={this.props.user.name} />
-        <ProfileLink username={this.props.user.username} />
-      </div>
-    );
-  }
-
-});
+var routes = require('./config/routes');
 
 ReactDOM.render(
-  <Avatar user={USER_DATA} />, document.getElementById('app')
+  routes, document.getElementById('app')
 )

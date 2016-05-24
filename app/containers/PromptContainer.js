fd6c552af3a0bb9cf5812a68aefc7eb919237352
Michał Ordon <michal@tehidea.com>
2016-05-24 22:01:46 +0100

    Add PromptContainer

---
+++ app/containers/PromptContainer.js
@@ -0,0 +1,26 @@
+var React = require('react');
+
+var PromptContainer = React.createClass({
+
+  render: function() {
+    console.log(this);
+    return (
+      <div className="jumbotron col-sm-6 col-sm-offset-3 text-center">
+        <h1>{this.props.route.header}</h1>
+        <div className="col-sm-12">
+          <form action="">
+            <div className="form-group">
+              <input type="text" className="form-control" placeholder="GitHub Username" />
+            </div>
+            <div className="form-group col-sm-4 col-sm-offset-4">
+              <button className="btn btn-block btn-success" type="Submit">Continue</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    );
+  }
+
+});
+
+module.exports = PromptContainer;

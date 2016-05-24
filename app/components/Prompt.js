5acd1c54eb2af8640520635060c590b2a9eb1a3e
Michał Ordon <michal@tehidea.com>
2016-05-24 23:50:29 +0100

    Turn Prompt into stateless functional component

--- app/components/Prompt.js
+++ app/components/Prompt.js
@@ -2,38 +2,34 @@ var React = require('react');
 var PropTypes = React.PropTypes;
 var transparentBg = require('../styles').transparentBg;
 
-var Prompt = React.createClass({
-
-  propTypes: {
-    onSubmitUser: PropTypes.func.isRequired,
-    onUpdateUser: PropTypes.func.isRequired,
-    header: PropTypes.string.isRequired,
-    username: PropTypes.string.isRequired
-  },
-
-  render: function() {
-    return (
-      <div className="jumbotron col-sm-6 col-sm-offset-3 text-center" style={transparentBg}>
-        <h1>{this.props.header}</h1>
-        <div className="col-sm-12">
-          <form action="" onSubmit={this.props.onSubmitUser}>
-            <div className="form-group">
-              <input
-                type="text"
-                className="form-control"
-                placeholder="GitHub Username"
-                onChange={this.props.onUpdateUser}
-                value={this.props.username} />
-            </div>
-            <div className="form-group col-sm-4 col-sm-offset-4">
-              <button className="btn btn-block btn-success" type="Submit">Continue</button>
-            </div>
-          </form>
-        </div>
+function Prompt(props) {
+  return (
+    <div className="jumbotron col-sm-6 col-sm-offset-3 text-center" style={transparentBg}>
+      <h1>{props.header}</h1>
+      <div className="col-sm-12">
+        <form action="" onSubmit={props.onSubmitUser}>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="GitHub Username"
+              onChange={props.onUpdateUser}
+              value={props.username} />
+          </div>
+          <div className="form-group col-sm-4 col-sm-offset-4">
+            <button className="btn btn-block btn-success" type="Submit">Continue</button>
+          </div>
+        </form>
       </div>
-    );
-  }
+    </div>
+  );
+}
 
-});
+Prompt.propTypes = {
+  onSubmitUser: PropTypes.func.isRequired,
+  onUpdateUser: PropTypes.func.isRequired,
+  header: PropTypes.string.isRequired,
+  username: PropTypes.string.isRequired
+},
 
 module.exports = Prompt;

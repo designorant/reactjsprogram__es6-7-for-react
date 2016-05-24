5f4e104b3f89a0a80c8ba8dfb1a308519796af20
Michał Ordon <michal@tehidea.com>
2016-05-24 22:31:02 +0100

    Add some action to the PromptContainer

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,10 +1,19 @@
 var React = require('react');
+var transparentBg = require('../styles').transparentBg;
+var ReactRouter = require('react-router');
+var Link = ReactRouter.Link;
 
 var Home = React.createClass({
 
   render: function() {
     return (
-      <div>Hello from Home!</div>
+      <div className="jumbotron col-sm-12 text-center" style={transparentBg}>
+        <h1>GitHub Battle</h1>
+        <p className="lead">Some fancy motto</p>
+        <Link to='/playerOne'>
+          <button type="button" className="btn btn-lg btn-success">Get Started</button>
+        </Link>
+      </div>
     );
   }
 
--- app/containers/PromptContainer.js
+++ app/containers/PromptContainer.js
@@ -3,15 +3,55 @@ var transparentBg = require('../styles').transparentBg;
 
 var PromptContainer = React.createClass({
 
+  contextTypes: {
+    router: React.PropTypes.object.isRequired
+  },
+
+  getInitialState: function() {
+    return {
+      username: ''
+    };
+  },
+
+  onUpdateUser: function(e) {
+    this.setState({
+      username: e.target.value
+    });
+  },
+
+  onSubmitUser: function(e) {
+    e.preventDefault();
+    var username = this.state.username;
+    this.setState({
+      username: ''
+    });
+
+    if (this.props.routeParams.playerOne) {
+      this.context.router.push({
+        pathname: '/battle',
+        query: {
+          playerOne: this.props.routeParams.playerOne,
+          playerTwo: this.state.username
+        }
+      })
+    } else {
+      this.context.router.push('/playerTwo/' + this.state.username)
+    }
+  },
+
   render: function() {
-    console.log(this);
     return (
       <div className="jumbotron col-sm-6 col-sm-offset-3 text-center" style={transparentBg}>
         <h1>{this.props.route.header}</h1>
         <div className="col-sm-12">
-          <form action="">
+          <form action="" onSubmit={this.onSubmitUser}>
             <div className="form-group">
-              <input type="text" className="form-control" placeholder="GitHub Username" />
+              <input
+                type="text"
+                className="form-control"
+                placeholder="GitHub Username"
+                onChange={this.onUpdateUser}
+                value={this.state.username} />
             </div>
             <div className="form-group col-sm-4 col-sm-offset-4">
               <button className="btn btn-block btn-success" type="Submit">Continue</button>

d5233dec08638e2da6dd5dd6ea089009f5697cad
Michał Ordon <michal@tehidea.com>
2016-05-25 15:38:07 +0100

    Fix a typo

--- app/utils/gitHubHelpers.js
+++ app/utils/gitHubHelpers.js
@@ -2,7 +2,7 @@ var axios = require('axios');
 
 var id = 'YOUR_CLIENT_ID';
 var sec = 'YOUR_SECRET_ID';
-var param = '?client_id' + id + '&client_secret=' + sec;
+var param = '?client_id=' + id + '&client_secret=' + sec;
 
 function getUserInfo(username) {
   return axios.get('https://api.github.com/users/' + username + param)

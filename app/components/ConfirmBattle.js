2132c0e0f1c50152d29656bfae6c36d3b5c3041d
Michał Ordon <michal@tehidea.com>
2016-05-25 14:18:10 +0100

    Get GitHub data

--- app/components/ConfirmBattle.js
+++ app/components/ConfirmBattle.js
@@ -1,10 +1,14 @@
 var React = require('react');
 
+function puke(object) {
+  return <pre>{JSON.stringify(object, null, ' ')}</pre>
+}
+
 function ConfirmBattle(props) {
 
   return props.isLoading === true
     ? <p>Loading</p>
-    : <p>Confirm battle!</p>
+    : <p>Confirm battle! {puke(props)}</p>
 
 };
 
--- app/containers/ConfirmBattleContainer.js
+++ app/containers/ConfirmBattleContainer.js
@@ -1,5 +1,6 @@
 var React = require('react');
 var ConfirmBattle = require('../components/ConfirmBattle');
+var gitHubHelpers = require('../utils/gitHubHelpers');
 
 var ConfirmBattleContainer = React.createClass({
 
@@ -22,7 +23,13 @@ var ConfirmBattleContainer = React.createClass({
   componentDidMount: function() {
     console.log('componentDidMount')
     var query = this.props.location.query;
-    // Fetch info from GitHub, and update state
+    gitHubHelpers.getPlayersInfo([query.playerOne, query.playerTwo])
+      .then(function(players) {
+        this.setState({
+          isLoading: false,
+          playersInfo: [players[0], players[1]]
+        })
+      }.bind(this))
   },
 
   componentWillReceiveProps: function(nextProps) {

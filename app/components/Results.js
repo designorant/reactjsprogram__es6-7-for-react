6fcce0234d189159f74525cd35852050da132898
Michał Ordon <michal@tehidea.com>
2016-05-27 23:49:05 +0100

    Update Results

--- app/components/Results.js
+++ app/components/Results.js
@@ -1,13 +1,24 @@
 var React = require('react');
 var PropTypes = React.PropTypes;
-
-function puke(object) {
-  return <pre>{JSON.stringify(object, null, ' ')}</pre>
-}
+var styles = require('../styles');
+var UserDetails = require('./UserDetails');
+var UserDetailsWrapper = require('./UserDetailsWrapper');
 
 function Results(props) {
+  var winningIndex = props.scores[0] > props.scores[1] ? 0 : 1;
+  var losingIndex = winningIndex === 0 ? 1 : 0;
   return (
-    <div>Results {puke(props)}</div>
+    <div className="jumbotron col-sm-12 text-center" style={styles.transparentBg}>
+      <h1>Results</h1>
+      <div className="col-sm-8 col-sm-offset-2">
+        <UserDetailsWrapper header="Winner">
+          <UserDetails score={props.scores[winningIndex]} info={props.playersInfo[winningIndex]} />
+        </UserDetailsWrapper>
+        <UserDetailsWrapper header="Winner">
+          <UserDetails score={props.scores[losingIndex]} info={props.playersInfo[losingIndex]} />
+        </UserDetailsWrapper>
+      </div>
+    </div>
   )
 }
 

233087b1ef9fb6ad4d08158a594b84e55159aff6
Michał Ordon <michal@tehidea.com>
2016-05-29 23:59:33 +0100

    Add Loading

--- app/components/Results.js
+++ app/components/Results.js
@@ -5,6 +5,7 @@ var UserDetails = require('./UserDetails');
 var UserDetailsWrapper = require('./UserDetailsWrapper');
 var Link = require('react-router').Link;
 var MainContainer = require('./MainContainer.js');
+var Loading = require('./Loading');
 
 function StartOver() {
   return (
@@ -20,7 +21,7 @@ function Results(props) {
 
   if (props.isLoading === true) {
     return (
-      <p>Loading</p>
+      <Loading text='One moment' />
     )
   }
 

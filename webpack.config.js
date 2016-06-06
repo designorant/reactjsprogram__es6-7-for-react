6d0657f79cd6ad1f777c8eed620a33901b17599b
Michał Ordon <michal@tehidea.com>
2016-06-06 17:39:04 +0100

    Add CSS loader and some transitions

--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,10 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         loader: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader'
       }
     ]
   },
